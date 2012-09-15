764dc0c0413fef4fd79391a233df6c50732430b9
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2012-09-15 04:46:35 +0400

    minor adjustments to config reading
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- cvinputstream.cpp
+++ cvinputstream.cpp
@@ -41,16 +41,10 @@ int cvInputStream::loadConfigFromLua(lua_State *L, String instance) {
   int h = lua_tonumber(L,-1);
   lua_pop(L,1);
 
-  lua_pushstring(L, "fps");
+  lua_pushstring(L, "maxfps");
   lua_gettable(L, -2);
   assert(lua_isnumber(L,-1));
   int fps = lua_tonumber(L,-1);
-  lua_pop(L,1);
-
-  lua_pushstring(L, "framedelay");
-  lua_gettable(L, -2);
-  assert(lua_isnumber(L,-1));
-  int framedelay = lua_tonumber(L,-1);
   lua_pop(L,1);
 
   lua_pushstring(L, "debug");
@@ -64,12 +58,24 @@ int cvInputStream::loadConfigFromLua(lua_State *L, String instance) {
   assert(lua_isnumber(L,-1));
   int camid = lua_tonumber(L,-1);
   lua_pop(L,1);
+  
+  lua_pushstring(L, "cascadeprofiling");
+  lua_gettable(L, -2);
+  assert(lua_isbolean(L,-1));
+  int cprofile = lua_tobolean(L,-1);
+  lua_pop(L,1);
 
   printf("cvInputStream: %s (%d) %dx%d\n", instance.data(), camid, w, h);
-  printf("cvInputStream: %d fps with %d ms delay\n", fps, framedelay);
+  printf("cvInputStream: Adjusting delays to match %d fps\n", fps);
   capture = cvCaptureFromCAM(camid);
   cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, w); 
   cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, h); 
+  if (debug) {
+    String wname = String("cvInputStream: ") + instance;
+    printf("cvInputStream: Debug enabled to window '%s'\n", wname.data());
+    cvNamedWindow( wname.data(), 1 );
+    }
+  sleep(100);
   /* Now we have to load cascades from config */ 
   
 }
--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ print(" === Necromant's facelock daemon 0.1, entering Lua Environment === \n");
 
 face = {
    cascades = {"./haarcascade_frontalface_alt2.xml", "./haarcascade_profileface.xml"}, 
+   maxsize = { 30, 30 },
+   nearonjs = 4,
    enabled = true,
    debug = true, 
 }
@@ -10,10 +12,11 @@ frontcam = {
    camid = 0, 
    w = 320,
    h = 240,
-   fps = 25,
+   maxfps = 5,
    framedelay = 100,
    enabled = true,
    debug = true,
+   cascadeprofiling=true,
    detect = { "face" },
 }
 
